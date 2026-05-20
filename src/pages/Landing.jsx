3a4443d251dc5aed356a574320a6eaa9cafaf08a
guilherrmehhh-coder <guilherrmehhh@gmail.com>
2026-05-19 23:41:04 -0300

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -356,16 +356,5 @@ const Landing = () => {
     </div>
   );
 };
-// pages/brandos.js
-import { useEffect } from 'react';
-
-export default function BrandosRedirect() {
-  useEffect(() => {
-    window.location.href =
-      'https://quasar-clover-232.notion.site/BrandOS-3668f4281cc0804e9676c8adb9819f2b';
-  }, []);
-
-  return null;
-}
 
 export default Landing;
