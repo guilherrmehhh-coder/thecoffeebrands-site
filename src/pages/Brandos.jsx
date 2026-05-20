5daee343c898a52dfe0fabff6b78a297c74cc0c4
guilherrmehhh-coder <guilherrmehhh@gmail.com>
2026-05-19 23:43:09 -0300

    brandos.jsx

---
+++ src/pages/Brandos.jsx
@@ -0,0 +1,11 @@
+// src/pages/Brandos.jsx
+import { useEffect } from 'react';
+
+export default function BrandosRedirect() {
+  useEffect(() => {
+    window.location.href =
+      'https://quasar-clover-232.notion.site/BrandOS-3668f4281cc0804e9676c8adb9819f2b';
+  }, []);
+
+  return null;
+}
