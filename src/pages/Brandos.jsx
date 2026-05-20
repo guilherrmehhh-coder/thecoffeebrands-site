fe84c6e17f9441a25b610f07ce960fc65f27ef88
guilherrmehhh-coder <guilherrmehhh@gmail.com>
2026-05-19 23:44:38 -0300

    Update Brandos.jsx

--- src/pages/Brandos.jsx
+++ src/pages/Brandos.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 export default function BrandosRedirect() {
   useEffect(() => {
     window.location.href =
-      'https://quasar-clover-232.notion.site/BrandOS-3668f4281cc0804e9676c8adb9819f2b';
+      https://quasar-clover-232.notion.site/BrandOS-3668f4281cc0804e9676c8adb9819f2b;
   }, []);
 
   return null;
